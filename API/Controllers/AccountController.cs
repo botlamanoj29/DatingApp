3b08151eb282c792aa80e6e2608ceb9df49d360e
botlamanoj29 <botlamanoj29@gmail.com>
2023-07-09 16:14:13 -0700

    End of Section 4

---
+++ API/Controllers/AccountController.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs;
+using API.Entities;
+using API.Interfaces;
+using API.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+ 
+    public class AccountController : BaseApiController
+    {
+        private readonly DataContext _context;
+        private ITokenService _tokenservice;
+        public AccountController(DataContext context, ITokenService tokenService)
+        {
+            _context=context;
+            _tokenservice=tokenService;
+        }
+
+        [HttpPost("register")]
+        
+        public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
+        {
+            if(await UserExists(registerDto.UserName)) return BadRequest ("Username is taken");
+
+            using var hmac = new HMACSHA512();
+            var user = new AppUser{
+                UserName=registerDto.UserName,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+                PasswordSalt = hmac.Key
+            };
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return new UserDto {UserName=user.UserName,
+            Token=_tokenservice.CreateToken(user)};
+
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
+        {
+            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName==loginDto.UserName);
+
+            if(user == null)
+            {return Unauthorized("Invalid Username");}
+
+            
+            using var hmac = new HMACSHA512(user.PasswordSalt);
+            var computedHash= hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+            for(int i=0; i< computedHash.Length; i++)
+            {
+                if(computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
+            }
+            return new UserDto {UserName=user.UserName,
+            Token=_tokenservice.CreateToken(user)};
+
+        }
+
+        public async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName==username);
+        }
+        
+    }
+}
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title: string = 'Dating app';
+  users: any; 
+
+  constructor(private htpp: HttpClient) {}
+  
+  ngOnInit(): void {
+    this.htpp.get('https://localhost:5001/api/Users').subscribe({
+      next: response => this.users=response,
+      error: error => console.log(error),       
+      complete: () => {}
+    })
+  }
+  
+}
