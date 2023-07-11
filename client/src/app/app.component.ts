c12da7b018d3b19611a0bd1f135c0f8136204d19
botlamanoj29 <botlamanoj29@gmail.com>
2023-07-11 08:49:14 -0700

    End of Section 5

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { AccountService } from './_services/account.service';
+import { User } from './_models/user';
+
 
 @Component({
   selector: 'app-root',
@@ -8,16 +11,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   title: string = 'Dating app';
-  users: any; 
 
-  constructor(private htpp: HttpClient) {}
-  
+
+  constructor(private accountservice: AccountService) { }
+
   ngOnInit(): void {
-    this.htpp.get('https://localhost:5001/api/Users').subscribe({
-      next: response => this.users=response,
-      error: error => console.log(error),       
-      complete: () => {}
-    })
+
+    this.setCurrentUser();
   }
-  
+
+
+
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user: User = JSON.parse(userString);
+    this.accountservice.setCurrentUser(user);
+
+  }
+
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,15 +4,30 @@ import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import {DropdownModule} from "ngx-dropdown";
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    DropdownModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
